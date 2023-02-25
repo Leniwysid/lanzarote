596daa2262877ed10eea61bade833983b5e8c82d
Patryk <leniwysid@gmail.com>
2023-02-25 13:35:08 +0100

    Update CitiesCard.jsx

--- src/components/CitiesCard.jsx
+++ src/components/CitiesCard.jsx
@@ -5,7 +5,7 @@ const CitiesCard = (props) => {
     <div className="relative">
       <img className="w-full h-full object-cover" src={props.bg} alt="/" />
       <div className="bg-gray-900/10 absolute top-0 left-0 w-full h-full">
-        <p className="left-4 bottom-4 text-2xl font-bold text-white absolute">
+        <p className="left-4 bottom-1 text-2xl font-bold text-white absolute">
           {props.text}
         </p>
       </div>
